be9013fdcfb761f2c49b477367364fa86a673abf
Jaroslav Hejný <jaroslavhejny@email.cz>
2026-02-05 14:09:36 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import * as React from "react";
 import {generateGrid} from "./helpers/generateGrid.ts";
 import {useState} from "react";
 import {Box} from "./components/Box.tsx";
+import './App.css'
 
 
 const App: React.FC = () => {
     const [grid, _] = useState(generateGrid(7, 7))
     return (
-        <div>
-            {grid.map((row, rowIndex) =>
-                row.map((_, columnIdex) => (
-                    <Box rowIndex={rowIndex} columnIndex={columnIdex}/>
-                )))}
+        <div className={'grid'}>
+            {grid.map(
+                (row, rowIndex) =>
+                    row.map(
+                        (_, columnIndex) => (
+                            <Box rowIndex={rowIndex} columnIndex={columnIndex}/>
+                        )
+                    )
+            )}
         </div>
     )
 }
